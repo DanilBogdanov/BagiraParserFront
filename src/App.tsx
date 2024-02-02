4510d5add4bc8436680e5f1cdd1c5eb15668123f
DanilWork <bogdanovdanila1404@gmail.com>
2024-02-02 14:34:06 +0500

    feat: add layout to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import AppRouter from '@/router/AppRouter';
+import AppLayout from '@/components/layout/AppLayout';
+import ruRU from 'antd/locale/ru_RU';
 import 'antd/dist/reset.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ConfigProvider locale={ruRU}>
+        <BrowserRouter>
+          <AppLayout>
+            <AppRouter />
+          </AppLayout>
+        </BrowserRouter>
+      </ConfigProvider>
+    </>
+  );
 }
 
 export default App;
