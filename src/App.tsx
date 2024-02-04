79b073f51c901e882a19074f29a23b5c4f460edd
Danil HP <bogdanovdanila1404@gmail.com>
2024-02-04 19:54:35 +0500

    chore: install react-query

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import AppRouter from '@/router/AppRouter';
 import AppLayout from '@/components/layout/AppLayout';
 import ruRU from 'antd/locale/ru_RU';
 import 'antd/dist/reset.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const client = new QueryClient();
 
 function App() {
   return (
     <>
       <ConfigProvider locale={ruRU}>
         <BrowserRouter>
-          <AppLayout>
-            <AppRouter />
-          </AppLayout>
+          <QueryClientProvider client={client}>
+            <AppLayout>
+              <AppRouter />
+            </AppLayout>
+          </QueryClientProvider>
         </BrowserRouter>
       </ConfigProvider>
     </>
