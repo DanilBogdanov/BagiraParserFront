dbfeaccf53e6215c59e8ca3bd4211625bd68fbc5
DanilWork <bogdanovdanila1404@gmail.com>
2024-02-14 16:47:22 +0500

    feat: add pages queries

---
+++ src/queries/pagesQueries.ts
@@ -0,0 +1,88 @@
+import { getParserCompanies } from '@/api/competitorsApi';
+import {
+  createParserPage,
+  deleteParserPage,
+  getParserPages,
+  setParserPageStatus,
+  updateParserPage,
+} from '@/api/parserPagesApi';
+import { ParserPage } from '@/types';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+const STALE_TIME = 1000 * 60 * 10;
+
+export const useCompaniesQuery = () =>
+  useQuery({
+    queryKey: ['parser-companies'],
+    queryFn: getParserCompanies,
+    staleTime: STALE_TIME,
+  });
+
+export const usePagesQuery = (companyId: number) =>
+  useQuery({
+    queryKey: ['parser-pages', companyId],
+    enabled: !!companyId,
+    staleTime: STALE_TIME,
+    queryFn: () => getParserPages(companyId),
+  });
+
+export const useCreatePageMutation = (companyId: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (parserPage: ParserPage) => createParserPage(parserPage),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['parser-pages', companyId],
+      });
+    },
+  });
+};
+
+export const useUpdatePageMutation = (companyId: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (parserPage: ParserPage) => updateParserPage(parserPage),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['parser-pages', companyId],
+      });
+    },
+  });
+};
+
+export const useStatusPageMutation = (companyId: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      parserCompanyId,
+      pageId,
+      isActive,
+    }: {
+      parserCompanyId: number;
+      pageId: number;
+      isActive: boolean;
+    }) => setParserPageStatus(parserCompanyId, pageId, isActive),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['parser-pages', companyId],
+      });
+    },
+  });
+};
+
+export const useDeletePageMutation = (companyId: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (parserPage: ParserPage) =>
+      deleteParserPage(parserPage.parserCompanyId, parserPage.id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['parser-pages', companyId],
+      });
+    },
+  });
+};
