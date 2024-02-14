0a3f1cea678a0a0a1765996f58a821883a195220
Danil <bogdanovdanila1404@gmail.com>
2024-02-14 13:26:29 +0500

    Revert "Add layout"

--- src/pages/CompetitorsPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function CompetitorsPage() {
-  return (
-    <>
-      <div>Competitors</div>
-    </>
-  );
-}
--- src/pages/PricesPage.tsx
+++
@@ -1,9 +0,0 @@
-import { AppShellAside, AppShellSection, ScrollArea } from '@mantine/core';
-
-export default function PricesPage() {
-  return (
-    <AppShellAside style={{ left: 0, right: 'unset' }}>
-      <AppShellSection grow component={ScrollArea}></AppShellSection>
-    </AppShellAside>
-  );
-}
