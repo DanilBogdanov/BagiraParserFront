4d4748c8f0258742f75ccbfaed66910a62a58d16
DanilWork <bogdanovdanila1404@gmail.com>
2024-02-15 16:55:43 +0500

    feat: add basic ParserGoodTable

---
+++ src/components/tables/parserGoodTable/ParserGoodTable.tsx
@@ -0,0 +1,9 @@
+import { ParserGood } from '@/types/competitors';
+
+type ParserGoodTableProps = {
+  goods?: ParserGood[];
+};
+
+export default function ParserGoodTable({}: ParserGoodTableProps) {
+  return <></>;
+}
