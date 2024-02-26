6f0508bc55c374b706b94b3c5ebd5d7da8639f11
DanilWork <bogdanovdanila1404@gmail.com>
2024-02-26 16:27:10 +0500

    refactor: update goodMenu type

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,16 +7,8 @@ export enum AppPages {
 export interface GoodMenu {
   id: number;
   name: string;
-  children: GoodMenu[] | null;
-}
-
-export interface GoodMenuItem {
-  id: number;
-  name: string;
   path: string;
-  key: string;
-  label: string;
-  children: GoodMenuItem[] | null;
+  children: GoodMenu[] | null;
 }
 
 export interface BagiraGoodNameDto {
