bedbd1c9b73fdb7b1318ae90a28fae97616638ad
DanilWork <bogdanovdanila1404@gmail.com>
2024-02-27 13:43:59 +0500

    feat: add getBagiraGoods to api

--- src/api/pricesApi.ts
+++ src/api/pricesApi.ts
@@ -1,4 +1,5 @@
-import { GoodMenu } from '@/types/prices';
+import { GoodResponse } from '@/types';
+import { BagiraGood, BagiraGoodRequest, GoodMenu } from '@/types/prices';
 import axios from 'axios';
 
 const BASE_URL = 'https://danildev.net';
@@ -9,4 +10,19 @@ export async function getGoodMenu(): Promise<GoodMenu[]> {
   const { data: goodMenu } = await axios.get<GoodMenu[]>(path);
 
   return goodMenu;
+}
+
+export async function getBagiraGoods(
+  bagiraGoodRequest: BagiraGoodRequest
+): Promise<GoodResponse<BagiraGood>> {
+  const path = `${BASE_URL}/api/parser/v1/bagira/goods`;
+
+  const { data: bagiraGoods } = await axios.get<GoodResponse<BagiraGood>>(
+    path,
+    {
+      params: bagiraGoodRequest,
+    }
+  );
+
+  return bagiraGoods;
 }
