0086493bdd3a45a06b1a43833bb2c0353ddc4e8f
DanilWork <bogdanovdanila1404@gmail.com>
2024-02-27 13:13:43 +0500

    feat: add type BagiraGood

--- src/types/prices.tsx
+++ src/types/prices.tsx
@@ -1,6 +1,15 @@
+import { ParserGood } from './competitors';
+
 export interface GoodMenu {
   id: number;
   name: string;
   path: string;
   children: GoodMenu[] | null;
+}
+
+export interface BagiraGood {
+  id: number;
+  name: string;
+  price: number;
+  parserGoods: ParserGood[];
 }
